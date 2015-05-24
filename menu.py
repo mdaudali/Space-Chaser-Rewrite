fa63d2733b4cd0d5c7dd35c622556ae3d8aa6c06
mohammed <mdaudali786@gmail.com>
2015-05-24 17:01:09 +0100

    menu

--- menu.py
+++ menu.py
@@ -1,1 +1,11 @@
 __author__ = 'mohammed'
+import pygame
+pygame.init()
+size = width, height = 888, 660
+screen = pygame.display.set_mode(size)
+menu = pygame.image.load("menuart.png").convert()
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: pygame.quit()
+    screen.blit(menu, [0,0])
+    pygame.display.flip()
